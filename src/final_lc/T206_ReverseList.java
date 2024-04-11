ac0c43f6d28aa35aa36832b880318251ac4b9c0a
lehangmeng <lehang.meng@airiche.com>
2024-04-11 14:35:38 +0800

    206 反转链表

--- src/final_lc/T206_ReverseList.java
+++ src/final_lc/T206_ReverseList.java
@@ -5,11 +5,36 @@ public class T206_ReverseList {
         if (head == null) {
             return null;
         }
-
-        return head;
+        if (head.next == null) {
+            return head;
+        }
+        ListNode prev = head;
+        ListNode current = head.next;
+        if (current.next == null) {
+            current.next = prev;
+            head.next = null;
+            return current;
+        }
+        while (current != null ) {
+            ListNode temp = current.next;
+            current.next = prev;
+            prev = current;
+            current = temp;
+        }
+        head.next = null;
+        return prev;
     }
 
     public static void main(String[] args) {
-
+        ListNode node = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+//        ListNode node3 = new ListNode(3);
+//        ListNode node4 = new ListNode(4);
+//        ListNode node5 = new ListNode(5);
+        node.next = node2;
+//        node2.next = node3;
+//        node3.next = node4;
+//        node4.next = node5;
+        System.out.println(reverseList(node));
     }
 }
